726c47d56a1b756731ab62069d30a300e4a365bc
jdwilkin4 <jwilkin4@hotmail.com>
2025-11-08 23:55:38 -0800

    feat: adding navbar desktop styles

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { Brain } from "lucide-react";
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <header className="py-6 border-b-1 border-gray-600 flex items-center justify-around">
+      <div className="logo-container flex gap-2">
+        <Brain size={30} />
+        <p className="text-2xl">DSA Visualized</p>
+      </div>
+      <nav>
+        <ul className="flex gap-6 text-lg">
+          <li>
+            <Link
+              className="hover:text-purple-600 ease-out duration-300 transition-all"
+              href="/concepts"
+            >
+              Concepts
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-purple-600 ease-out duration-300 transition-all"
+              href="/data-structures"
+            >
+              Data Structures
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-purple-600 ease-out duration-300 transition-all"
+              href="/algorithms"
+            >
+              Algorithms
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-purple-600 ease-out duration-300 transition-all"
+              href="/challenges"
+            >
+              Challenges
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:text-purple-600 ease-out duration-300 transition-all"
+              href="/about"
+            >
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
